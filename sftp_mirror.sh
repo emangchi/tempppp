a96593cda8ca4797e0105584bd52fca55cd588d9
emangchi <ssank31@naver.com>
2022-02-10 16:20:26 +0900

    Create sftp_mirror.sh

---
+++ sftp_mirror.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+SERVICE="lftp"
+if pgrep -x "$SERVICE" >/dev/null
+then
+    logger "$SERVICE is already running"
+else
+    LastUploadFilePath="./LastUploadFileInfo.txt"
+
+    for SourceFilePath in "./files"/*.json
+    do
+        [ -f "$SourceFilePath" ] || continue
+
+        rm -f $LastUploadFilePath
+
+        logger "upload start $SourceFilePath"
+
+        SourceFileSize=$(ls -al $SourceFilePath | awk '{print $5}')
+        logger "SourceFileSize is $SourceFileSize"
+
+        SourceFileName=$(basename $SourceFilePath)
+
+        SFTPCMD_DEBUG="lftp -d sftp://dbs_env_vib_test:1234@s-6795fc9179ba4bb29.server.transfer.ap-northeast-2.amazonaws.com \
+        -e 'set sftp:connect-program \"ssh -i id_rsa\";\
+        set net:timeout 10;set net:max-retries 1;set net:reconnect-interval-multiplier 1;set net:reconnect-interval-base 1;\
+        set xfer:log-file /honglim/sftp-xfer.log;\
+        set log:file /honglim/sftp-proto.log;\
+        put -e ${SourceFilePath};\
+        cls -l ${SourceFileName} | xargs > ${LastUploadFilePath}
+        bye'"
+
+        SFTPCMD="lftp sftp://dbs_env_vib_test:1234@s-6795fc9179ba4bb29.server.transfer.ap-northeast-2.amazonaws.com \
+        -e 'set sftp:connect-program \"ssh -i id_rsa\";\
+        set net:timeout 10;set net:max-retries 1;set net:reconnect-interval-multiplier 1;set net:reconnect-interval-base 1;\
+        put -e ${SourceFilePath};\
+        cls -l ${SourceFileName} | xargs > ${LastUploadFilePath}
+        bye'"
+
+        eval "$SFTPCMD_DEBUG"
+        result=$?
+
+        if [ $result -eq 0 ]; then
+            if [ -f "$LastUploadFilePath" ]; then
+                UploadedFileSize=$(cat $LastUploadFilePath | awk '{print $5}')
+                logger "UploadedFileSize  is $UploadedFileSize"                  
+                if [ "$UploadedFileSize" = "$SourceFileSize" ]; then             
+                    logger "upload success & delete file :${SourceFilePath}"     
+                    rm -rf $SourceFilePath                                       
+                else                                                             
+                    logger "UploadedFileSize  error"                             
+                fi 
+            else
+                logger "Create fail $LastUploadFilePath" 
+            fi
+        else
+            logger "upload fail :${SourceFilePath}"
+        fi
+    done
+fi
